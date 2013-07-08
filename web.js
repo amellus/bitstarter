a4abb47ae0078a9043c57695fb3127d347846a41
Ubuntu <ubuntu@ip-172-31-45-113.us-west-2.compute.internal>
2013-07-08 14:57:17 +0000

    Fixed bug in web.js

--- web.js
+++ web.js
@@ -4,8 +4,9 @@ var buf = require('buffer');
 
 var app = express.createServer(express.logger());
 
-var msg_buf = fs.readFileSync(index.html);
+var msg_buf = fs.readFileSync("index.html");
 var msg_str = msg_buf.toString();
+
 
 app.get('/', function(request, response) {
   response.send(msg_str);
